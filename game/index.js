ace6248c91437e5ef201b87d8d866efecf4b6fa0
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-10 08:41:35 -0500

    Updated index.js

--- game/index.js
+++ game/index.js
@@ -1,14 +1,18 @@
+import { randomComputerChoice } from "./randComChoice.js";
 
-    const rock = document.getElementById('rock');
-    const paper = document.getElementById('paper');
-    const scissors = document.getElementById('scissors');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 
-    console.log(rock, paper, scissors); // Debugging: Check if elements are correctly selected
+export var userChoice = document.getElementById('user-choice');
+export var computerChoice = document.getElementById('com-choice');
+export const endingResult = document.getElementById('ending-result');
 
-    let wpns = [rock, paper, scissors];
+export let wpns = [rock, paper, scissors];
 
-    wpns.forEach(wpn => {
-      wpn.addEventListener('click', () => {
-        console.cog(wpn.id);
-      })
-    });
+wpns.forEach(wpn => {
+  wpn.addEventListener('click', () => {
+      userChoice.textContent = wpn.id;
+      randomComputerChoice();
+  });
+});
