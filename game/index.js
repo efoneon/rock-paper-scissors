762561d6459e2d69ba9f17436f6e992a44f8d84a
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-10 08:59:14 -0500

    Added a "rules" section to determine winner
    Also changed "ending-result" element id to "end-result" (correct id)

--- game/index.js
+++ game/index.js
@@ -6,7 +6,7 @@ const scissors = document.getElementById('scissors');
 
 export var userChoice = document.getElementById('user-choice');
 export var computerChoice = document.getElementById('com-choice');
-export const endingResult = document.getElementById('ending-result');
+export const endingResult = document.getElementById('end-result');
 
 export let wpns = [rock, paper, scissors];
 
@@ -16,3 +16,25 @@ wpns.forEach(wpn => {
       randomComputerChoice();
   });
 });
+
+function userWin() {
+  if (userChoice.id === rock && computerChoice.id === scissors ||
+      userChoice.id === paper && computerChoice.id === rock ||
+      userChoice.id === scissors && computerChoice.id === paper) {
+          endingResult.textContent = 'You win!';
+  }
+}
+
+function computerWin() {
+  if (userChoice.id === rock && computerChoice.id === paper ||
+      userChoice.id === paper && computerChoice.id === scissors ||
+      userChoice.id === scissors && computerChoice.id === rock) {
+          endingResult.textContent = 'Computer wins!';
+  }
+}
+
+function draw() {
+  if (userChoice.id === computerChoice.id) {
+      endingResult.textContent = 'Draw!';
+  }
+}
