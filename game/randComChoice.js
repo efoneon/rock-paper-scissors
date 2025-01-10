ef45a7ab8e09697d06aae7bf4c1b0f301eef45c0
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-10 09:59:29 -0500

    Deleted randComChoice.js

--- game/randComChoice.js
+++
@@ -1,7 +0,0 @@
-import { wpns, computerChoice } from './index.js';
-
-export function randomComputerChoice() {
-    const randomIndex = Math.floor(Math.random() * wpns.length);
-    const randomChoice = wpns[randomIndex];
-    computerChoice.textContent = randomChoice.id;
-}
