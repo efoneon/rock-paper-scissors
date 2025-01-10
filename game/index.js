d196cbaf888950ddba45ce73b8ac44496bae52e7
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-10 09:59:13 -0500

    Did a major update on index.js
    Re-did the game logic for the game

--- game/index.js
+++ game/index.js
@@ -1,47 +1,44 @@
-import { randomComputerChoice } from "./randComChoice.js";
+const choices = ['rock', 'paper', 'scissors']
 
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-
-export const userChoice = document.getElementById('user-choice');
-export const computerChoice = document.getElementById('computer-choice');
-export const endingResult = document.getElementById('ending-result');
-export const wpns = [rock, paper, scissors];
-
-wpns.forEach(wpn => {
-  wpn.addEventListener('click', () => {
-      userChoice.textContent = wpn.id;
-      randomComputerChoice();
-      checkWinner();
-  });
-});
-
-function checkWinner() {
-  const userValue = userChoice.textContent;
-  const computerValue = computerChoice.textContent;
-
-  if (userValue === computerValue) {
-    endingResult.textContent = "It's a tie!"
-  } else if (userValue === 'rock' && computerValue === 'scissors' ||
-      userValue === 'paper' && computerValue === 'rock' ||
-      userValue === 'scissors' && computerValue === 'paper') {
-          endingResult.textContent = 'You win!';
-  } else {
-    endingResult.textContent = 'Computer wins!';
-  }
+function getRandomChoice() {
+  return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function computerWin() {
-  if (userChoice.id === rock && computerChoice.id === paper ||
-      userChoice.id === paper && computerChoice.id === scissors ||
-      userChoice.id === scissors && computerChoice.id === rock) {
-          endingResult.textContent = 'Computer wins!';
-  }
+function getWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) return 'draw'
+    
+    if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        return 'user'
+    } else {
+        return 'computer'
+    }
 }
 
-function draw() {
-  if (userChoice.id === computerChoice.id) {
-      endingResult.textContent = 'Draw!';
-  }
-}
+document.getElementById('weapon-choice-container').addEventListener('click', (e) => {
+    if (e.target.tagName !== 'BUTTON') return
+
+    // Get player choices.
+    const userChoice = e.target.id
+    const computerChoice = getRandomChoice()
+    
+    // Put player choices on the screen.
+    document.getElementById('user-choice').textContent = userChoice
+    document.getElementById('computer-choice').textContent = computerChoice
+    
+    // Determine which player won.
+    const winner = getWinner(userChoice, computerChoice)
+    const endingResultEl = document.getElementById('ending-result')
+
+    // Display the winner.
+    if (winner === 'draw') {
+        endingResultEl.textContent = 'Draw!'
+    } else if (winner === 'user') {
+        endingResultEl.textContent = 'You win!'
+    } else {
+        endingResultEl.textContent = 'Computer wins!'
+    }
+})
