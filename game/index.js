f1f69cbd2de8f0dba02d91c8e59dfcec6e874aff
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-10 09:28:53 -0500

    Updated logic in index.js

--- game/index.js
+++ game/index.js
@@ -4,24 +4,31 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
-export var userChoice = document.getElementById('user-choice');
-export var computerChoice = document.getElementById('com-choice');
-export const endingResult = document.getElementById('end-result');
-
-export let wpns = [rock, paper, scissors];
+export const userChoice = document.getElementById('user-choice');
+export const computerChoice = document.getElementById('computer-choice');
+export const endingResult = document.getElementById('ending-result');
+export const wpns = [rock, paper, scissors];
 
 wpns.forEach(wpn => {
   wpn.addEventListener('click', () => {
       userChoice.textContent = wpn.id;
       randomComputerChoice();
+      checkWinner();
   });
 });
 
-function userWin() {
-  if (userChoice.id === rock && computerChoice.id === scissors ||
-      userChoice.id === paper && computerChoice.id === rock ||
-      userChoice.id === scissors && computerChoice.id === paper) {
+function checkWinner() {
+  const userValue = userChoice.textContent;
+  const computerValue = computerChoice.textContent;
+
+  if (userValue === computerValue) {
+    endingResult.textContent = "It's a tie!"
+  } else if (userValue === 'rock' && computerValue === 'scissors' ||
+      userValue === 'paper' && computerValue === 'rock' ||
+      userValue === 'scissors' && computerValue === 'paper') {
           endingResult.textContent = 'You win!';
+  } else {
+    endingResult.textContent = 'Computer wins!';
   }
 }
 
